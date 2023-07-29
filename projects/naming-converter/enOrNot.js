798e02e7ae5e801dd8f2fff08bfb2d14b27b4a50
smeemarket <smeemarket@gmai.com>
2023-07-29 20:32:14 +0630

    error keyboard

--- projects/naming-converter/enOrNot.js
+++ projects/naming-converter/enOrNot.js
@@ -7,36 +7,42 @@ function genCharArray(charA, charZ) {
     }
     return a;
 }
-function converter(inputValue) {
+function converter(x) {
+    x.value = x.value.toUpperCase();
+    inputValue = x.value;
     if (genCharArray("a", "z").includes(inputValue.toLowerCase().at(0))) {
         document.getElementById('from').innerText = 'English';
         document.getElementById('to').innerText = 'Myanmar';
         result = en2mm(inputValue);
-        return result;
+        document.getElementById('output').innerText = result;
     } else if (genCharArray('က', 'အ').includes(inputValue.at(0))) {
         document.getElementById('from').innerText = 'Myanmar';
         document.getElementById('to').innerText = 'English';
         result = mm2en(inputValue);
-        return result;
+        document.getElementById('output').innerText = result;
     } else {
         document.getElementById('from').innerText = 'Myanmar';
         document.getElementById('to').innerText = 'English';
-        return "SAW MIN OO";
+        document.getElementById('input').innerText = "စောမင်းဦး";
+        document.getElementById('output').innerText = "SAW MIN OO";
     }
 }
 
-function langChange(from) {
+function langChange(x) {
+    const from = document.getElementById('from').innerText;
     if (from === 'Myanmar') {
         document.getElementById('from').innerText = 'English';
         document.getElementById('to').innerText = 'Myanmar';
         document.getElementById('input').value = '';
         document.getElementById('input').placeholder = 'SAW MIN OO';
         document.getElementById('output').innerText = 'စောမင်းဦး';
+        // x.style.backgroundColor = 'blue';
     } else {
         document.getElementById('from').innerText = 'Myanmar';
         document.getElementById('to').innerText = 'English';
         document.getElementById('input').value = '';
         document.getElementById('input').placeholder = 'စောမင်းဦး';
         document.getElementById('output').innerText = 'SAW MIN OO';
+        // x.style.backgroundColor = 'red';
     }
 }
