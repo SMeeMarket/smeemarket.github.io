1a7eca29be69d97d336ddcb5260bee273b358a5f
smeemarket <smeemarket@gmai.com>
2023-09-07 12:13:35 +0630

    html mail

--- folder/projects/naming-converter/buttonSound.js
+++
@@ -1,8 +0,0 @@
-const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
-const buttons = document.querySelectorAll("button");
-
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        audio.play();
-    });
-});
